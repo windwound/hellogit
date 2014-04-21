bb86f2226ec70eab6dd0082d45e802ea4301328d
windwound <qjd2rd@gmail.com>
2014-04-21 11:52:33 +0800

    ok
    add some method

--- hellogit/hellogit/hellogit.cpp
+++ hellogit/hellogit/hellogit.cpp
@@ -4,14 +4,36 @@
 #include "stdafx.h"
 #include <iostream>
 #include <assert.h>
+#include <string>
 using namespace std;
 
 template<typename T> inline size_t _sizeof(T p)
 {
 	return (char*)(&p+1)-(char*)(&p);
 }
+char* _strcpy(char* dst, const char* src) 
+{
+	assert(dst !=NULL || src != NULL);
+	char* r = dst;
+	while ((*dst++ = *src++) != '\0');
+
+	return r;
+}
+size_t _strlen(const char* str) 
+{
+	size_t len = 0;
+	while(*str++)
+		++len;
+	return len;
+
+}
 
 class MString{
+	friend ostream& operator <<(ostream& out, const MString& str)
+	{
+		out<<str.m_pBuffer;
+		return out;
+	}
 public:
 	MString(const char* str = NULL)
 	{
@@ -21,12 +43,12 @@ public:
 		}
 		else
 		{
-			size_t len = _strlen(str);  
-			m_pBuffer = new char[len + 1];
+			m_nLen = _strlen(str);  
+			m_pBuffer = new char[m_nLen + 1];
 			_strcpy(m_pBuffer, str);
 		};
 	}
-	MString(const MString& str):m_pBuffer(NULL)
+	MString(const MString& str):m_pBuffer(NULL),m_nLen(0)
 	{
 		*this = str;
 	}
@@ -38,9 +60,9 @@ public:
 	{
 		if(this == &str)
 			return *this;
-		size_t len = _strlen(str.m_pBuffer);
+		m_nLen = str.m_nLen;
 
-		char* tmp = new char[len + 1];
+		char* tmp = new char[m_nLen + 1];
 		_strcpy(tmp, str.m_pBuffer);
         delete[] m_pBuffer;
 
@@ -48,45 +70,28 @@ public:
 
 		return *this;
 	}
-	operator char* ()
+	inline int GetLength() const
+	{
+		return m_nLen;
+	}
+	char& operator [](int i) 
+	{
+		return m_pBuffer[i];
+	}
+	const char& operator [](int i) const
+	{
+		return m_pBuffer[i];
+	}
+	const char* c_str() const
 	{
 		return m_pBuffer;
 	}
-	size_t GetLength()
-	{
-		return _strlen(m_pBuffer);
-	}
 private:
 	char* m_pBuffer;
-protected:
-	char* _strcpy(char* dst, const char* src)
-	{
-		assert(dst !=NULL || src != NULL);
-		char* r = dst;
-		while ((*dst++ = *src++) != '\0');
-		
-		return r;
-	}
-	size_t _strlen(const char* str)
-	{
-		size_t len = 0;
-		while(*str++)
-			++len;
-		return len;
-		
-	}
+	size_t m_nLen;
 };
-
 int _tmain(int argc, _TCHAR* argv[])
 {
-	MString str = "test",t = "t";
-	MString str2 = str;
-	str2 = t;
-
-	cout<<str2<<endl;
-
-	cout<<str2.GetLength();
-	cin.get();
-	return 0;
+	
 }
 
